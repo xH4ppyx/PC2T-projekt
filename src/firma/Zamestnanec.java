2743915f35d9a8e7d19f66a4586f02afa9ee17d5
David Kovář <270956@vutbr.cz>
2026-03-30 09:07:35 +0200

    add

--- src/firma/Zamestnanec.java
+++ src/firma/Zamestnanec.java
@@ -1,5 +1,28 @@
 package firma;
 
-public abstract class Zamestnanec {
+import java.util.HashMap;
+import java.util.Map;
 
+public abstract class Zamestnanec {
+    private int id;
+    private String jmeno;
+    private String prijmeni;
+    private int rokNarozeni;
+
+    // Dynamická datová struktura pro evidenci spolupráce (ID kolegy -> úroveň spolupráce) 
+    private Map<Integer, String> spolupracovnici;
+
+    public Zamestnanec(int id, String jmeno, String prijmeni, int rokNarozeni) {
+        this.id = id;
+        this.jmeno = jmeno;
+        this.prijmeni = prijmeni;
+        this.rokNarozeni = rokNarozeni;
+        // Inicializace mapy (zde budeme ukládat např. klíč 5 = "dobrá")
+        this.spolupracovnici = new HashMap<>();
+    }
+
+    // Abstraktní metoda pro specifické dovednosti skupin [cite: 14, 25]
+    public abstract void spustDovednost();
+    
+    // Zbytek (gettery, settery) přidáme později...
 }
