4e97bc415a2c0cb7a6117fcc874e4293f58ba097
Oliver Synak <281728@vutbr.cz>
2026-04-20 22:21:30 +0200

    added interface, 2 new classes for file/sql database in the future

---
+++ database_project/src/firma/FileEmployeeRepositor.java
@@ -0,0 +1,10 @@
+package firma;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+public class FileEmployeeRepositor implements EmployeeRepository {
+
+	
+	
+}
---
+++ database_project/src/firma/SQLEmployeeRepository.java
@@ -0,0 +1,11 @@
+package firma;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+
+public class SQLEmployeeRepository implements EmployeeRepository {
+	
+	public void saveFile() {
+		
+	}
+}
