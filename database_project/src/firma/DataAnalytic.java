fd4b9e393a8ab7019dba14402261305eaf775936
Kovy <96984543+Lidong9@users.noreply.github.com>
2026-05-05 09:35:00 +0200

    Refactor startSkill method for clarity and efficiency

--- database_project/src/firma/DataAnalytic.java
+++ database_project/src/firma/DataAnalytic.java
@@ -12,7 +12,7 @@ public class DataAnalytic extends Employee {
 
     @Override
     public void startSkill() {
-    	Map<Integer, CoopQuality> myCoworkers = this.getCoworkers();
+        Map<Integer, CoopQuality> myCoworkers = this.getCoworkers();
 
         if (myCoworkers.isEmpty()) {
             System.out.println("Nemám žádné spolupracovníky pro analýzu.");
@@ -23,18 +23,33 @@ public class DataAnalytic extends Employee {
         Employee bestMatch = null;
         
         for (Integer coworkerId : myCoworkers.keySet()) {
-        	Employee coworker = CompanyManager.getAllEmployees().get(coworkerId);
-        	
-        	if (coworker == null) {
-        		continue;
-        	}
-        	Set<Integer> commonCoworkers = new HashSet<>(myCoworkers.keySet());
-        	commonCoworkers.retainAll(coworker.getCoworkers().keySet());
-        	commonCoworkers.remove(this.getId());
+            Employee coworker = CompanyManager.getAllEmployees().get(coworkerId);
+            
+            if (coworker == null) {
+                continue;
+            }
+            
 
-        	int commonCount = commonCoworkers.size();
-        	
+            Set<Integer> commonCoworkers = new HashSet<>(myCoworkers.keySet());
+
+            commonCoworkers.retainAll(coworker.getCoworkers().keySet());
+
+            commonCoworkers.remove(this.getId());
+
+            int commonCount = commonCoworkers.size();
+            
+            if (commonCount > maxCommon) {
+                maxCommon = commonCount;
+                bestMatch = coworker;
+            }
         }
         
+        if (bestMatch != null) {
+            System.out.println("Spolupracovník s nejvíce společnými vazbami je: " 
+                + bestMatch.getName() + " " + bestMatch.getSurname() 
+                + " (ID: " + bestMatch.getId() + ") s celkem " + maxCommon + " společnými spolupracovníky.");
+        } else {
+            System.out.println("Nepodařilo se najít žádného spolupracovníka ke srovnání.");
+        }
     }
 }
